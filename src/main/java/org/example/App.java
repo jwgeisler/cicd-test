b80ed5f4f240634284988d8ee3f568ad9721cd75
Jgeisler <jwgeisler@gmail.com>
2020-10-30 15:45:42 -0700

    jfjf

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldjhgssdfss!" );
+        System.out.println( "Hello Worldjhgssdss!" );
     }
 }
